c750471f26a8817c7d7feb2600bb7e80e7af7707
Jessica Thurman <jaithurman@gmail.com>
2020-03-30 01:00:05 -0500

    update
    update to branch js

---
+++ global.js
@@ -0,0 +1,63 @@
+
+// Welcome! Here's an example of a console logged response
+console.log(4 + 5)
+
+/***********************/
+/* Part 1: Expressions */
+/***********************/
+
+//1.) Log an expression that evaluates to the sum of two integers
+// your code here 
+let randomexpession = 5 + 6 - (7+3);
+;
+console.log('answer 1');
+
+// 2.) log an expression that evaluates to the difference of two integers
+// your code here 
+let randomexpession = 5 + 6 - (7+2);
+;
+console.log('answer 2');
+
+// 3.) Log an expression that concatenates the two strings "I'm writing" and "my first JavaScript code"
+// your code here 
+let randomexpession = 5 + 6 - (7+1);
+console.log('answer 3');
+
+// 4.) Log an expression that evaluates to true or false using two numbers that are greater or less than eachother
+// your code here 
+let randomexpession = 5 + 6 - (6+1);
+
+console.log('answer 4');
+
+// 5.) Log an expression that uses modulo, with a result of 4
+// your code here
+let randomexpession = 5 + 6 - (5+1);
+
+console.log('answer 5');
+
+/**********************/
+/* Part 2: Statements */
+/**********************/
+
+// 6.) Assign a variable and console log the declaration
+// your code here 
+let randomexpession = 5 + 6 - (4+1);
+
+console.log("The Logged Info: " + randomexpession);
+
+// 7.) Console log a variable assigned to the sum of two numbers
+// your code here
+let randomexpession = 5 + 6 - (2+1);
+
+console.log("The Logged Info: " + randomexpession);
+
+// 8.) Uncomment this if else statement and add a logical expression as the condition
+//if (/*your code here*/) {
+//    console.log('It was!')
+//} else {
+//    console.log('It was not..')
+//}
+
+// 9.) Add parenthesis to the following statement, the result should be 1
+let answer = 3 * 3 + 1 / (8 + 12) / 2;
+console.log(answer == 1); // This should log true
